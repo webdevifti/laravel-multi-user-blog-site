929afd4ce4f1e69f555497428dcec80a1544038c
webdevifti <eftialam@hotmail.com>
2022-01-04 11:48:40 +0600

    404 page created

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>404 | Page not found</title>
+    <style>
+        .wrapper{
+            height: 100vh;
+            width: 100%;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+        }
+     
+        .hgroup{text-transform:uppercase;}
+        .hgroup h1{margin-bottom:25px; font-size:80px;}
+        .hgroup h1 span{display:inline-block; margin-left:5px; padding:2px; border:1px solid #CCCCCC; overflow:hidden;}
+        .hgroup h1 span strong{display:inline-block; padding:0 20px 20px; border:1px solid #CCCCCC; font-weight:normal;}
+        .hgroup h2{font-size:60px;}
+        .hgroup h2 span{display:block; font-size:30px;}
+        p{margin:25px 0 0 0; padding:0; font-size:16px;}
+        p:first-child{margin-top:0;}
+        a{background-color: #3F83F8;color: #fff;padding: 10px 15px;border-radius: 20px;text-decoration: none;font-size: 2rem;font-weight: bold;}
+    </style>
+</head>
+<body>
+    <div class="wrapper">
+        <div class="clear">
+         
+          <section " class="clear">
+          
+            <div class="hgroup">
+              <h1><span><strong>4</strong></span><span><strong>0</strong></span><span><strong>4</strong></span></h1>
+              <h2>Error ! <span>Page Not Found</span></h2>
+            </div>
+            <p>For Some Reason The Page You Requested Could Not Be Found On Our Server</p>
+            <p> <a  href="/">Go Home &raquo;</a></p>
+            <!-- ####################################################################################################### -->
+          </section>
+      
+        </div>
+      </div>
+</body>
+</html>
